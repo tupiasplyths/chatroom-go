70209d5a9d716c90e8d03b0e6a698a5713b00d53
tupiasplyths <20021409@vnu.edu.vn>
2024-04-12 16:35:10 +0700

    try fixing host name

--- client/src/wsContext.js
+++ client/src/wsContext.js
@@ -1,11 +1,12 @@
 import { createContext, useContext } from "react";
-
+import dotenv from 'dotenv';
+dotenv.config();
 export const WebSocketContext = createContext(null);
-
+const backend_url = process.env.BACKEND_URL; 
 export const SocketProvider = ({ children, username }) => {
     // const [socket, setSocket] = useState(null);
     
-    const socket = new WebSocket("ws://localhost:3789/ws?name=" + username);
+    const socket = new WebSocket("ws://" + backend_url + ":3789/ws?name=" + username);
     return (
         <WebSocketContext.Provider value={socket}>{children}</WebSocketContext.Provider>
 
